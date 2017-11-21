e2c62aa715adf9a0e745a089ea8d809dc9fcfb5e
mbouluad <mbouluad@finaxys.com>
2017-11-21 16:53:15 +0100

    app task list

---
+++ src/main/java/com/tasklistservice/Task.java
@@ -0,0 +1,28 @@
+package com.tasklistservice;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Task {
+    private long id;
+
+    private String content;
+
+    public Task() {
+        // Jackson deserialization
+    }
+
+    public Task(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @JsonProperty
+    public long getId() {
+        return id;
+    }
+
+    @JsonProperty
+    public String getContent() {
+        return content;
+    }
+}
